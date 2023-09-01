46eada878189a41b8d44c97172f5b09adbe23727
Pete <pete.bankhead@gmail.com>
2023-09-01 07:10:27 +0100

    Add preference annotations

---
+++ src/main/java/qupath/fx/prefs/annotations/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Annotations to assist with defining preferences.
+ * <p>
+ * This is designed primarily to help create preference panes using a ControlsFX
+ * {@link org.controlsfx.control.PropertySheet}, using externalized strings for
+ * the labels and tooltips.
+ * <p>
+ * The annotations are used by applying them to JavaFX properties.
+ */
+package qupath.fx.prefs.annotations;
